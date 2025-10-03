9f949927ab62ee8ce61bbf005f2fc3ee041386c0
andre <andre.abdirrosyid@gmail.com>
2025-10-04 05:22:57 +0700

    Add register form

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,18 @@
 require("dotenv").config();
 const express = require("express");
 const mongoose = require("mongoose");
+const cors = require("cors");
 const authRoutes = require("./src/routes/auth.js");
 const userRoutes = require("./src/routes/user.js");
 
 const app = express();
+
+app.use(cors({
+    origin: ["http://localhost:5173", "http://127.0.0.1:5173"],
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    credentials: true
+}))
+
 app.use(express.json());
 
 mongoose
@@ -18,6 +26,7 @@ app.get("/", (req, res) => {
 
 app.use("/api/auth", authRoutes);
 app.use("/api/user", userRoutes);
+
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server at http://localhost:${PORT}`));
--- frontend/src/pages/Register.tsx
+++ frontend/src/pages/Register.tsx
@@ -1,5 +1,62 @@
+import React, { useState } from "react";
+import axios from "axios";
+
+
 function Register() {
-    return <h2>REGISTER PAGE</h2>
+    const [name, setName] = useState("");
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    const [message, setMessage] = useState("");
+
+    const handleSubmit = async(e: React.FormEvent) => {
+        e.preventDefault();
+
+        try {
+            const res = await axios.post("http://localhost:5000/api/auth/register", {       
+                name,
+                email,
+                password
+            });
+
+            setMessage(res.data.message);
+        } catch (err: any) {
+            setMessage(err.response?.data?.message || "Something went wrong");
+        }
+    };
+
+    return (
+        <div>
+        <h2>REGISTER PAGE</h2>
+        <form onSubmit={handleSubmit}>
+            <input
+                type="text"
+                placeholder="Name"
+                value={name}
+                onChange={(e) => setName(e.target.value)}
+                required
+            />
+            <br />
+            <input
+                type="email"
+                placeholder="Email"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+            />
+            <br />
+            <input
+                type="password"
+                placeholder="Password"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+            />
+            <br />
+            <button type="submit">REGISTER</button>
+        </form>
+
+        {message && <p>{message}</p>}
+        </div>
+    )
 }
+
 
 export default Register;
